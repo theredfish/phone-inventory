439a2f5651362e5bdfb4f1f224e5eff1a98139db
Marina Blin <marina.blin@free.fr>
2015-10-08 08:53:33 +0200

    [Comments] Adding comments for Accessoire's class

--- Telephonie/src/inventaire/Accessoire.java
+++ Telephonie/src/inventaire/Accessoire.java
@@ -3,7 +3,7 @@
  * 
  * Description de la classe : Accessoire
  * 	Classe fille de la classe Article.
- * 	Classe abstraite permettant de dÈfinir les diffÈrents caractÈristiques d'un accessoire.
+ * 	Classe abstraite permettant de d√©finir les diff√©rents caract√©ristiques d'un accessoire.
  */
 
 package inventaire;
@@ -12,43 +12,55 @@ import java.util.*;
 
 public abstract class Accessoire extends Article 
 {
-	// Definition attributs
+	// D√©finition attributs
 	private TreeSet<Marque> marquesCompatibles;
 
-	// Definition constructeur
+	// D√©finition constructeur
 	/**
 	 * Constructeur de la classe Accessoire
 	 * 
-	 * @param reference : String - rÈfÈrence d'un article
-	 * @param intitule : String - intitulÈ d'un article
+	 * @param reference : String - r√©f√©rence d'un article
+	 * @param intitule : String - intitul√© d'un article
 	 * @param prix : double - prix d'un article
-	 * @param marquesCompatibles : TreeSet<Marque> - marques associÈes ‡ l'article
+	 * @param marquesCompatibles : TreeSet<Marque> - marques associ√©es √† l'article
 	 */
-	protected Accessoire(String reference, String intitule, double prix, TreeSet<Marque> marquesCompatibles) {
+	protected Accessoire(String reference, String intitule, double prix, TreeSet<Marque> marquesCompatibles) 
+	{
 		super(reference, intitule, prix);
 		this.marquesCompatibles = marquesCompatibles;
 	}
 	
-	// Definition getter and setter
-	public TreeSet<Marque> getMarquesCompatibles() {
+	// D√©finition getter and setter
+	/**
+	 * Getter permettant de connaitre la liste des accessoires
+	 * @return La liste des accessoire - TreeSet<Marque>
+	 */
+	public TreeSet<Marque> getMarquesCompatibles() 
+	{
 		return marquesCompatibles;
 	}
 
-	public void setMarquesCompatibles(TreeSet<Marque> marquesCompatibles) {
+	/**
+	 * Setter permettant de changer la liste des accessoires par la liste marquesCompatibles
+	 * @param marquesCompatibles : TreeSet<Marque> - Liste des accessoires
+	 */
+	public void setMarquesCompatibles(TreeSet<Marque> marquesCompatibles) 
+	{
 		this.marquesCompatibles = marquesCompatibles;
 	}
 
-	// Definition method (process?)
-	public String toString() {
+	// D√©finition des m√©thodes
+	public String toString() 
+	{
 		String result = super.toString() + " est compatible avec les marques suivantes :\n";
 		
-		if (!marquesCompatibles.isEmpty()) {
-			for (Marque marque : marquesCompatibles) 
-			{
+		if (!marquesCompatibles.isEmpty()) 
+		{
+			for (Marque marque : marquesCompatibles)
 				result += "- " + marque.toString() +"\n";
-			}
 		}
-		else {
+		else 
+		{
 			result += "aucune";
 		}
 		
